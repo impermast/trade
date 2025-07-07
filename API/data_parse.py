895c895c5bb1c0fa965c721b79940dd1ecf068ca
Margonchik <marlivge@gmail.com>
2025-07-07 20:45:01 +0300

    Parser/New release/ 1 def for all data

--- API/data_parse.py
+++ API/data_parse.py
@@ -5,11 +5,15 @@ import time
 import os
 
 
-def fetch_historical_data(exchange, symbol, start_date='2023-01-01T00:00:00Z', timeframe='1h', save_path=None):
-    """
-    Загрузка исторических OHLCV-данных.
-    """
-    print(f"Загружаем исторические данные {symbol} с {start_date} ({timeframe}) через {exchange.id}...")
+def fetch_data(exchange_id, symbol, timeframe='1h', start_date='2025-01-01T00:00:00Z', limit=100):
+
+    exchange_class = getattr(ccxt, exchange_id)
+    exchange = exchange_class({'enableRateLimit': True})
+
+    print(f"Загружаем данные {symbol} ({timeframe}) через {exchange_id}...")
+
+    if not start_date:
+        raise ValueError("Необходимо указать start_date.")
     all_data = []
     since = exchange.parse8601(start_date)
     now = exchange.milliseconds()
@@ -22,42 +26,13 @@ def fetch_historical_data(exchange, symbol, start_date='2023-01-01T00:00:00Z', t
         since = ohlcv[-1][0] + 1
         time.sleep(exchange.rateLimit / 1000.0)
 
-        print(f"Загружено {len(all_data)} записей...")
-
-    df = pd.DataFrame(all_data, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+    data = all_data
+    df = pd.DataFrame(data, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
     df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
 
-    if save_path:
-        os.makedirs(os.path.dirname(save_path), exist_ok=True)
-        df.to_csv(save_path, index=False)
-        print(f"Данные сохранены в {save_path}")
-
     return df
-
-
-def fetch_live_data(exchange, symbol, timeframe='1m', limit=100):
-    """
-    Получение последних свечей в реальном времени.
-    """
-    print(f"Получаем последние {limit} свечей {symbol} ({timeframe}) через {exchange.id}...")
-    ohlcv = exchange.fetch_ohlcv(symbol, timeframe=timeframe, limit=limit)
-    df = pd.DataFrame(ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
-    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
-    return df
-
 
 
 if __name__ == "__main__":
-    exchange = ccxt.bybit({'enableRateLimit': True})
-    symbol = 'BTC/USDT'
-    project_root = os.path.dirname(os.path.abspath(__file__))
-    data_dir = os.path.join(project_root, "..", "DATA")
-    data_dir = os.path.abspath(data_dir)
-    save_path = os.path.join(data_dir, "BTCUSDT_15m.csv")
-    # Исторические данные
-    df_hist = fetch_historical_data(exchange, symbol, start_date='2025-01-01T00:00:00Z',
-                                     timeframe='15m', save_path=save_path)
-
-    # Актуальные данные (например, для стратегии в реальном времени)
-    df_live = fetch_live_data(exchange, symbol, timeframe='1m', limit=50)
-    print(df_live.tail())
+    a = fetch_data("bybit", "BTC/USDT", timeframe="15m", start_date="2025-05-05T00:00:00Z")
+    print(a)

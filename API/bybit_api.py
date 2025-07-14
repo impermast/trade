db82bcb53b344d2b82f4c0c874bceec6459fa8e7
Margonchik <marlivge@gmail.com>
2025-07-14 11:56:01 +0300

    try to fix 2

--- API/bybit_api.py
+++ API/bybit_api.py
@@ -424,7 +424,7 @@ class BybitAPI(BirzaAPI):
 
 if __name__ == "__main__":
     bot = BybitAPI(api_key=None, api_secret=None)
-    bot.download_candels_to_csv("BTC/USDT", start_date="2023-05-05T00:00:00Z", timeframe="1h")
+    bot.download_candels_to_csv("BTC/USDT", start_date="2025-05-05T00:00:00Z", timeframe="1h")
     df = bot.get_ohlcv("BTC/USDT")
     # print(df.head)
 
